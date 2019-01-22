55c42fe1a2c462d56135d0f2347082afd53b9cf9
Derek Riddle <dmr62@students.uwf.edu>
2019-01-22 09:17:34 -0600

    fixed spacing for tiny monitor

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -27,10 +27,12 @@ namespace ConsoleApp1
             }
 
             Double myDouble1 = 54.898989;
-            Console.WriteLine("The double " + myDouble1 + " is " + MoreExamples.castDouleToInt(myDouble1) + " when casted to a int.  Notice it rounds down.");
+            Console.WriteLine("The double " + myDouble1 + " is " + MoreExamples.castDouleToInt(myDouble1) 
+                + " when casted to a int.  Notice it rounds down.");
 
             int myInt = 55;
-            Console.WriteLine("The int " + myInt + " is " + MoreExamples.castIntToDouble(myInt) + " when casted to a Double.  Notice it does not print any zeros.");
+            Console.WriteLine("The int " + myInt + " is " + MoreExamples.castIntToDouble(myInt) 
+                + " when casted to a Double.  Notice it does not print any zeros.");
 
             Console.ReadKey();
         }
